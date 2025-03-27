950b164b9f3ed3a0adb1434a7f8346e6b5e40eac
PedroHernandez173 <Phernande0152@gmail.com>
2025-03-27 14:11:54 -0700

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Student2 on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
